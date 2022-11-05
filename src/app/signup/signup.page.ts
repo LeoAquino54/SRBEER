7a164591dd25fd6d39e2c6949915735d52414af2
Leonardo Aquino <aquinoleonardo5484@gmail.com>
2022-11-05 15:08:36 -0300

    novo

--- src/app/signup/signup.page.ts
+++ src/app/signup/signup.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AlertController, LoadingController, NavController } from '@ionic/angular';
+import { alertController } from '@ionic/core';
 import { UserService } from '../services/user.service';
 
 @Component({
@@ -11,6 +13,9 @@ import { UserService } from '../services/user.service';
 export class SignupPage implements OnInit {
 formulario : FormGroup
   constructor(private router:Router,private formbuilder:FormBuilder,
+    public loadingController:LoadingController,
+    public alertController: AlertController,
+    public navController:NavController,
     private userService:UserService) { }
 
   ngOnInit() {
@@ -26,9 +31,19 @@ formulario : FormGroup
   signUPUser(){
  this.router.navigate(['login']);
   }
-  register(){
+  async register(){
+    const loading = await this.loadingController.create({
+      message: 'Salvando...',
+      translucent: true,
+      backdropDismiss: true,
+      cssClass: 'spinner-color-custon',
+    });
+    await loading.present();
     this.userService.register(this.formulario.value).subscribe(res => {
       console.log(res)
+      this.navController.navigateRoot("/home")
+      loading.dismiss()
+      // this.alertController.create("Salvo", "Usuario Cadastrado com sucesso");
     })
   }
 }
